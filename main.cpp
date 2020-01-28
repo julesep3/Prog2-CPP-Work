e4e90b43aaf450fe9bc67a04de56f3d75bd67ae3
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-01-28 13:32:52 -0600

    add comments in main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main()
 {
-	// Original Array Initialization
+	//  Generate 20 random numbers.
+	//  Print the random numbers.
 	int i;
 	int n = 20;
 	int ar[n];
@@ -19,17 +20,20 @@ int main()
 	cout << endl
 		 << endl;
 
-	// Bubble Sort Procedure
+
+	// Bubble Sort Procedure =====================================
 	int arBubble[n];
 	cout << "Bubble Sort" << endl;
-	
+	//   Move the original random numbers into another array.
 	for (i = 0; i < n; i++)
 	{
 		arBubble[i] = ar[i];
 		cout << arBubble[i] << ' ';
 	}
 	cout << endl;
+	//   Bubble Sort the other array.
 	int bubbleCost = Bubble(arBubble, n);
+	// Print the sorted numbers AND the  cost of sorting the numbers.
 	for (i = 0; i < n; i++)
 	{
 		cout << arBubble[i] << ' ';
@@ -38,16 +42,19 @@ int main()
 	cout << "Bubblesort costs " << bubbleCost << endl
 		 << endl;
 
-	// Shell Sort Procedure
+	// Shell Sort Procedure =========================================
 	int arShell[n];
 	cout << "Shell Sort" << endl;
+	//  Move the original random numbers into another array.
 	for (i = 0; i < n; i++)
 	{
 		arShell[i] = ar[i];
 		cout << arShell[i] << ' ';
 	}
 	cout << endl;
+	//  Shell Sort the other array.
 	int shellCost = Shell(arShell, n);
+	//  Print the sorted numbers AND the cost of sorting the numbers. 
 	for (i = 0; i < n; i++)
 	{
 		cout << arShell[i] << ' ';
@@ -56,16 +63,19 @@ int main()
 	cout << "Shellsort costs " << shellCost << endl
 		 << endl;
 
-	// Quick Sort Procedure
+	// Quick Sort Procedure ========================================
 	int arQuick[n];
 	cout << "Quick Sort" << endl;
+	//  Move the original random numbers into another array.
 	for (i = 0; i < n; i++)
 	{
 		arQuick[i] = ar[i];
 		cout << arQuick[i] << ' ';
 	}
 	cout << endl;
+	//  Quick Sort the other array.
 	int cost = Quick(arQuick, n);
+	//  Print the sorted numbers AND the cost of sorting the numbers. 
 	for (i = 0; i < n; i++)
 	{
 		cout << arQuick[i] << ' ';
