474637bbd88e5109ca26d61e2c8ba5b7f2f963e2
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-02-03 10:16:35 -0600

    Discrete Structures Proj1 start

---
+++ DSProj1/p01.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int problem1();
+int problem2();
+int problem3();
+int problem4();
+
+
+
+int main() {
+
+	/*
+	int a1 = problem1(p, q);
+	int a2 = problem2(p, q);
+	int a3 = problem3(p, q);
+	int a4 = problem4(p, q, r);
+	r = 0;
+	int b4 = problem4(p, q, r);
+	r = 1;
+	q = 0;
+	int b1 = problem1(p, q);
+	int b2 = problem2(p, q);
+	int b3 = problem3(p, q);
+	int c4 = problem4(p, q, r);
+	r = 0;
+	int d4 = problem4(p, q, r);
+	p = 0;
+	int d1 = problem1(p, q);
+	int d2 = problem2(p, q);
+	int d3 = problem3(p, q);
+	int h4 = problem4(p, q, r);
+	r = 1;
+	int g4 = problem4(p, q, r);
+	q = 1;
+	int c1 = problem1(p, q);
+	int c2 = problem2(p, q);
+	int c3 = problem3(p, q);
+	int e4 = problem4(p, q, r);
+	r = 0;
+	int f4 = problem4(p, q, r);
+	*/
+
+	return 0;
+}
+
+int problem1() {
+	int p = 1;
+	int q = 1;
+	int equality;
+
+
+
+	return 1;
+}
+int problem2() {
+	int p = 1;
+	int q = 1;
+	int equality;
+
+	return 1;
+}
+int problem3() {
+	int p = 1;
+	int q = 1;
+	int equality;
+
+	return 1;
+}
+int problem4() {
+	int p = 1;
+	int q = 1;
+	int r = 1;
+	int equality;
+
+	return 1;
+}
