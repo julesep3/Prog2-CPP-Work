8f8c2a32e93343ba145587d83aa13e582028ae6a
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-02-22 16:18:08 -0600

    getline trial

--- HW4.2/main.cpp
+++ HW4.2/main.cpp
@@ -10,92 +10,144 @@ Take input, convert into postfix
 #include <istream>
 #include <fstream>
 #include <string>
-#include <cctype>
 using namespace std;
 
 template <typename T>
-struct stack {
-	T stk [50];
+struct stack
+{
+	T stk[50];
 	int top = -1;
-	void push(T i) {
+	void push(T i)
+	{
 		stk[++top] = i;
 	}
-	void pop(T &i) {
+	void pop(T &i)
+	{
 		i = stk[top--];
 	}
-	int empty() {
+	int empty()
+	{
 		return top == -1;
 	}
-	int getTop() {
-		return top;
+	T getTop()
+	{
+		return stk[top];
 	}
-	void display() {
-		if (top >= 0) {
-			for (int i = 0; i <= top; i++) {
+	void display()
+	{
+		if (top >= 0)
+		{
+			for (int i = top; i >= 0; i--)
+			{
 				cout << stk[i] << " ";
 			}
 			cout << endl;
-		} else {
-			cout << "Stack is empty";
+		}
+		else
+		{
+			cout << "Stack is empty" << endl;
+			;
 		}
 	}
 };
-int priority(char c) {
+int priority(char c)
+{
 	int rank = 0;
-	if(c == '^') {
+	if (c == '^')
+	{
 		rank = 3;
-	} else if((c == '*') || (c == '/')) {
+	}
+	else if ((c == '*') || (c == '/'))
+	{
 		rank = 2;
-	} else if ((c == '+') || (c == '-')) {
+	}
+	else if ((c == '+') || (c == '-'))
+	{
 		rank = 1;
-	} else if (c == '(') {
+	}
+	else if (c == '(')
+	{
 		rank = 0;
 	}
 	return rank;
 }
 
-int main() {
+int main()
+{
 	string aline;
 	ifstream inData;
 	stack<int> iStack;
 	stack<char> cStack;
 	stack<char> output;
 
+	int itemp;
+	char ctemp;
+
 	inData.open("input.txt");
-	while (getline(inData, aline, ' ')) {
-		if (isdigit(aline[0])) {
-			output.push(aline[0]);
-		} else if (aline.length() == 2) {
-			if (aline[0] == '(') {
+	while (getline(inData, aline, ' '))
+	{
+		if (aline.length() == 4)
+		{
+			cout << aline[0] << " ";
+			//output.push(aline[0]);
+			//output.push(aline[3]);
+			cout << endl << aline[3] << " ";
+		}
+		else if (aline.length() == 2)
+		{
+			if (aline[0] == '(')
+			{
 				cStack.push(aline[0]);
 				iStack.push(aline[1]);
-			} else if (aline[1] == ')') {
+			} 
+			else if (aline[1] == ')')
+			{
 				iStack.push(aline[0]);
-				int q = cStack.getTop();
+				while (cStack.getTop() != '(') {
+					cStack.pop(ctemp);
+					//output.push(ctemp);
+					//cout << ctemp << " ";
+				}
+				cStack.pop(ctemp);
 				
-			}
+					
+			} /*else if(cStack.empty()) {
+				//int temp;
+				iStack.pop(temp);
+			} else if (isdigit(aline[0])) {
+			output.push(aline[0]);
+			} */
 		}
-		
 
-
-		
 		/*
 		cout << aline.length() << " || ";
-		if (aline.length() == 2) {
-			if (aline[0] == '(') {
+		if (aline.length() == 2)
+		{
+			if (aline[0] == '(')
+			{
 				cout << aline[1] << endl;
-			} else if (aline[1] == ')') {
+			}
+			else if (aline[1] == ')')
+			{
 				cout << aline[0] << endl;
 			}
 		}
-		else if(aline.length() == 4) {
+		else if (aline.length() == 4)
+		{
 			cout << aline << endl;
 		}
-		else if(aline.length() == 1) {
+		else if (aline.length() == 1)
+		{
 			cout << aline << endl;
 		} */
+
+
+		
 	}
 	inData.close();
+	cout << endl
+		 << endl;
 	output.display();
 	iStack.display();
+	cStack.display();
 }
